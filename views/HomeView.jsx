7dcc8fe6819a09d7442a48bd011165c8eda71680
Samson Emblad <23201902+samsonsin@users.noreply.github.com>
2024-02-09 11:28:10 +0100

    Added surfaces underneath items in homeview

--- views/HomeView.jsx
+++ views/HomeView.jsx
@@ -14,7 +14,7 @@ import { LinearGradient } from "expo-linear-gradient";
 export default function HomeView({ devices, navigation }) {
 	return (
 		<>
-			<ScrollView className=" w-full h-full p-2 flex flex-col gap-2 gap-x-0">
+			<ScrollView className=" w-full h-full p-2 flex flex-col gap-4 gap-x-0">
 				{devices.map((element) => {
 					return CustomCard(element, navigation);
 				})}
@@ -33,35 +33,37 @@ export default function HomeView({ devices, navigation }) {
 
 function CustomCard(data, navigation) {
 	return (
-		<LinearGradient
-			key={data.id}
-			className="rounded-lg h-32 flex-column justify-between p-3 pb-0"
-			colors={["#b3e3fc", "#e8f4fa"]}
-		>
-			<View className="flex flex-row justify-between items-center">
-				<View className="w-10"></View>
-				<Text className="text-lg">{data.name}</Text>
-				<IconButton
-					className=""
-					icon="dots-vertical"
-					mode="default"
-					onPress={() => navigation.navigate("Edit Device Screen")}
-				></IconButton>
-			</View>
-			<View className="flex flex-row h-1/2 justify-evenly items-end">
-				<IconButton
-					className="grow"
-					icon="power"
-					mode="outlined"
-					onPress={() => console.log("Pressed")}
-				></IconButton>
-				<IconButton
-					className="grow"
-					icon="power-off"
-					mode="outlined"
-					onPress={() => console.log("Pressed")}
-				></IconButton>
-			</View>
-		</LinearGradient>
+		<Surface elevation={2} key={data.id} className="rounded-lg">
+			<LinearGradient
+				key={data.id}
+				className="rounded-lg h-32 flex-column justify-between p-3 pb-0"
+				colors={["#b3e3fc", "#e8f4fa"]}
+			>
+				<View className="flex flex-row justify-between items-center">
+					<View className="w-10"></View>
+					<Text className="text-lg">{data.name}</Text>
+					<IconButton
+						className=""
+						icon="dots-vertical"
+						mode="default"
+						onPress={() => navigation.navigate("Edit Device Screen")}
+					></IconButton>
+				</View>
+				<View className="flex flex-row h-1/2 justify-evenly items-end">
+					<IconButton
+						className="grow"
+						icon="power"
+						mode="outlined"
+						onPress={() => console.log("Pressed")}
+					></IconButton>
+					<IconButton
+						className="grow"
+						icon="power-off"
+						mode="outlined"
+						onPress={() => console.log("Pressed")}
+					></IconButton>
+				</View>
+			</LinearGradient>
+		</Surface>
 	);
 }
