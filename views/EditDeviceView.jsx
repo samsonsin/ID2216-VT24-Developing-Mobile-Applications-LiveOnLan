7d86399ff243769417fefa663dcf94f07146ec16
Ludzor <ludzor02@gmail.com>
2024-02-23 13:58:14 +0100

    Settings look better/more in line with rest of themes

--- views/EditDeviceView.jsx
+++ views/EditDeviceView.jsx
@@ -10,70 +10,160 @@ import {
 	Surface,
 	Button,
 } from "react-native-paper";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function EditDeviceView(pwops) {
 	return (
-		<View className="bg-[#b3e3fc] py-6 h-full">
+		<View className="bg-[#fff] h-full">
             <>
 			<ScrollView className=" w-full h-full p-4 flex flex-col">
             <View className="">{/*Simple Options*/}
-                    <View className="flex-direction: column m-2">
-                        <Text className="text-[18px]">Device Name</Text>
-                        <TextInput className="text-[20px] h-[45px] p-[6px] border-2 border-[#000] rounded-md"></TextInput>
-                    </View>
-                    <View className="flex-direction: column m-2">
-                        <Text className="text-[18px]">MAC Address</Text>
-                        <TextInput className="text-[20px] h-[45px] p-[6px] border-2 border-[#000] rounded-md"></TextInput>
-                    </View>
+            
+                    
+                    <Surface
+                        elevation={2}
+                        className="rounded-lg top-2 left-2 mr-4">
+                        <LinearGradient
+                            className="rounded-lg h-28 flex-column justify-between p-3 pb-0"
+                            colors={["#b3e3fc", "#e8f4fa"]}>
+                            <View className="flex flex-col justify-between items-left">
+                                <Text className="text-lg">Device Name</Text>  
+                                <TextInput className="text-[20px] h-[45px] p-[6px] border-2 border-[#000] rounded-md"></TextInput>
+                            </View>
+                            <View className="flex flex-row h-1/2 justify-evenly items-end">
+                            </View>
+                        </LinearGradient>
+                    </Surface>
+                    
+                    <Surface
+                        elevation={2}
+                        className="rounded-lg top-2 left-2 mr-4 mt-4">
+                        <LinearGradient
+                            className="rounded-lg h-28 flex-column justify-between p-3 pb-0"
+                            colors={["#b3e3fc", "#e8f4fa"]}>
+                            <View className="flex flex-col justify-between items-left">
+                                <Text className="text-lg">MAC Address</Text>  
+                                <TextInput className="text-[20px] h-[45px] p-[6px] border-2 border-[#000] rounded-md"></TextInput>
+                            </View>
+                            <View className="flex flex-row h-1/2 justify-evenly items-end">
+                            </View>
+                        </LinearGradient>
+                    </Surface>
                 </View>
-                <View className="">{/*Advanced Options*/}
+                <View className="mt-6 mb-2">{/*Advanced Options*/}
                     
                     {pwops.show
                     ? 
                     <>
+                    <View className="bg-[#b3e3fc] mt-3 mb-5 ml-2 mr-2 h-2 rounded-lg"></View>
                     <TouchableOpacity onPress={pwops.toggle}>{/*Dividing view*/}
-                        <View className="bg-[#60BCE0] mt-3 mb-3 ml-2 mr-2 h-2 rounded-lg"></View>
-                        <Text className="text-[30px] text-center">Hide Advanced Options</Text>
+                        <Text className="text-[30px] text-center border-[#000] bg-[#e8f4fa] border-2 rounded-lg">Hide Advanced Options</Text>
                     </TouchableOpacity>
-                        <View className="flex-direction: column m-2">
-                            <Text className="text-[18px]">Port</Text>
-                            <TextInput className="text-[20px] h-[45px] p-[6px] border-2 border-[#000] rounded-md"></TextInput>
-                        </View>
-                        <View className="flex-direction: column m-2">
-                            <Text className="text-[18px]">SecureOn Password</Text>
-                            <TextInput className="text-[20px] h-[45px] p-[6px] border-2 border-[#000] rounded-md"></TextInput>
-                        </View>
-                        <View className="flex-direction: column m-2">
-                            <Text className="text-[18px]">placeholder</Text>
-                            <TextInput className="text-[20px] h-[45px] p-[6px] border-2 border-[#000] rounded-md"></TextInput>
-                        </View>
-                        <View className="flex-direction: column m-2">
-                            <Text className="text-[18px]">placeholder</Text>
-                            <TextInput className="text-[20px] h-[45px] p-[6px] border-2 border-[#000] rounded-md"></TextInput>
-                        </View>
-                        <View className="flex-direction: column m-2">
-                            <Text className="text-[18px]">placeholder</Text>
-                            <TextInput className="text-[20px] h-[45px] p-[6px] border-2 border-[#000] rounded-md"></TextInput>
-                        </View>
-                        <View className="flex-direction: column m-2">
-                            <Text className="text-[18px]">placeholder</Text>
-                            <TextInput className="text-[20px] h-[45px] p-[6px] border-2 border-[#000] rounded-md"></TextInput>
-                        </View>
-                        <View className="flex-direction: column m-2">
-                            <Text className="text-[18px]">placeholder</Text>
-                            <TextInput className="text-[20px] h-[45px] p-[6px] border-2 border-[#000] rounded-md"></TextInput>
-                        </View>
-                        <View className="flex-direction: column m-2">
-                            <Text className="text-[18px]">placeholder</Text>
-                            <TextInput className="text-[20px] h-[45px] p-[6px] border-2 border-[#000] rounded-md"></TextInput>
-                        </View>
-                        <View className="h-[85px]"></View>
+                    <Surface
+                        elevation={2}
+                        className="rounded-lg top-2 left-2 mr-4 mt-4">
+                        <LinearGradient
+                            className="rounded-lg h-28 flex-column justify-between p-3 pb-0"
+                            colors={["#b3e3fc", "#e8f4fa"]}>
+                            <View className="flex flex-col justify-between items-left">
+                                <Text className="text-lg">Port</Text>  
+                                <TextInput className="text-[20px] h-[45px] p-[6px] border-2 border-[#000] rounded-md"></TextInput>
+                            </View>
+                            <View className="flex flex-row h-1/2 justify-evenly items-end">
+                            </View>
+                        </LinearGradient>
+                    </Surface>
+                    <Surface
+                        elevation={2}
+                        className="rounded-lg top-2 left-2 mr-4 mt-4">
+                        <LinearGradient
+                            className="rounded-lg h-28 flex-column justify-between p-3 pb-0"
+                            colors={["#b3e3fc", "#e8f4fa"]}>
+                            <View className="flex flex-col justify-between items-left">
+                                <Text className="text-lg">SecureOn Password</Text>  
+                                <TextInput className="text-[20px] h-[45px] p-[6px] border-2 border-[#000] rounded-md"></TextInput>
+                            </View>
+                            <View className="flex flex-row h-1/2 justify-evenly items-end">
+                            </View>
+                        </LinearGradient>
+                    </Surface>
+                    <Surface
+                        elevation={2}
+                        className="rounded-lg top-2 left-2 mr-4 mt-4">
+                        <LinearGradient
+                            className="rounded-lg h-28 flex-column justify-between p-3 pb-0"
+                            colors={["#b3e3fc", "#e8f4fa"]}>
+                            <View className="flex flex-col justify-between items-left">
+                                <Text className="text-lg">placeholder</Text>  
+                                <TextInput className="text-[20px] h-[45px] p-[6px] border-2 border-[#000] rounded-md"></TextInput>
+                            </View>
+                            <View className="flex flex-row h-1/2 justify-evenly items-end">
+                            </View>
+                        </LinearGradient>
+                    </Surface>
+                    <Surface
+                        elevation={2}
+                        className="rounded-lg top-2 left-2 mr-4 mt-4">
+                        <LinearGradient
+                            className="rounded-lg h-28 flex-column justify-between p-3 pb-0"
+                            colors={["#b3e3fc", "#e8f4fa"]}>
+                            <View className="flex flex-col justify-between items-left">
+                                <Text className="text-lg">placeholder</Text>  
+                                <TextInput className="text-[20px] h-[45px] p-[6px] border-2 border-[#000] rounded-md"></TextInput>
+                            </View>
+                            <View className="flex flex-row h-1/2 justify-evenly items-end">
+                            </View>
+                        </LinearGradient>
+                    </Surface>
+                    <Surface
+                        elevation={2}
+                        className="rounded-lg top-2 left-2 mr-4 mt-4">
+                        <LinearGradient
+                            className="rounded-lg h-28 flex-column justify-between p-3 pb-0"
+                            colors={["#b3e3fc", "#e8f4fa"]}>
+                            <View className="flex flex-col justify-between items-left">
+                                <Text className="text-lg">placeholder</Text>  
+                                <TextInput className="text-[20px] h-[45px] p-[6px] border-2 border-[#000] rounded-md"></TextInput>
+                            </View>
+                            <View className="flex flex-row h-1/2 justify-evenly items-end">
+                            </View>
+                        </LinearGradient>
+                    </Surface>
+                    <Surface
+                        elevation={2}
+                        className="rounded-lg top-2 left-2 mr-4 mt-4">
+                        <LinearGradient
+                            className="rounded-lg h-28 flex-column justify-between p-3 pb-0"
+                            colors={["#b3e3fc", "#e8f4fa"]}>
+                            <View className="flex flex-col justify-between items-left">
+                                <Text className="text-lg">placeholder</Text>  
+                                <TextInput className="text-[20px] h-[45px] p-[6px] border-2 border-[#000] rounded-md"></TextInput>
+                            </View>
+                            <View className="flex flex-row h-1/2 justify-evenly items-end">
+                            </View>
+                        </LinearGradient>
+                    </Surface>
+                    <Surface
+                        elevation={2}
+                        className="rounded-lg top-2 left-2 mr-4 mt-4">
+                        <LinearGradient
+                            className="rounded-lg h-28 flex-column justify-between p-3 pb-0"
+                            colors={["#b3e3fc", "#e8f4fa"]}>
+                            <View className="flex flex-col justify-between items-left">
+                                <Text className="text-lg">placeholder</Text>  
+                                <TextInput className="text-[20px] h-[45px] p-[6px] border-2 border-[#000] rounded-md"></TextInput>
+                            </View>
+                            <View className="flex flex-row h-1/2 justify-evenly items-end">
+                            </View>
+                        </LinearGradient>
+                    </Surface>
+                    <View className="h-[85px]"></View>
                     </>
                     :
                     <>
+                    <View className="bg-[#b3e3fc] mt-3 mb-5 ml-2 mr-2 h-2 rounded-lg"></View>
                     <TouchableOpacity onPress={pwops.toggle}>{/*Dividing view*/}
-                        <View className="bg-[#60BCE0] mt-3 mb-3 ml-2 mr-2 h-2 rounded-lg"></View>
-                        <Text className="text-[30px] text-center">Show Advanced Options</Text>
+                        <Text className="text-[30px] text-center border-[#000] bg-[#e8f4fa] border-2 rounded-lg">Show Advanced Options</Text>
                     </TouchableOpacity>
                     </>
                     }
