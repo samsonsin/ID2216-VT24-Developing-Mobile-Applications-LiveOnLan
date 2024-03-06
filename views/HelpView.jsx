8d2c0b2c107dd7c71fd2fd5f6e6dcede8dea5ae9
samsonsincd <23201902+samsonsin@users.noreply.github.com>
2024-03-06 21:34:29 +0100

    Text fixes on Help Screen

--- views/HelpView.jsx
+++ views/HelpView.jsx
@@ -18,12 +18,12 @@ export default function HelpView(pwops) {
 						If you're having issues with the app, have got some feedback or
 						otherwise would like to contact the Developer of this application;
 						then please contact us via this email address:
-						<Text
-							style={{ color: "blue" }}
-							onPress={() => Linking.openURL("mailto:emblad@kth.se")}
-						>
-							emblad@kth.se
-						</Text>
+					</Text>
+					<Text
+						style={{ color: "blue" }}
+						onPress={() => Linking.openURL("mailto:emblad@kth.se")}
+					>
+						emblad@kth.se
 					</Text>
 				</Surface>
 				<Surface
@@ -42,9 +42,8 @@ export default function HelpView(pwops) {
 						When a device wants to use Wake-on-Lan, it constructs a specific UDP
 						Datagram packet which is sent to either a destination IP address or
 						broadcast accross the local LAN. A recieving device, when shutdown,
-						will still investigate all arriving packets, and if a correctly
-						encoded WoL packet arrives, the networkdevice will power on the
-						device.
+						will still investigate all arriving packets. If a correctly encoded
+						WoL packet arrives, the NIC will power on the device.
 					</Text>
 				</Surface>
 				<Surface elevation={2} className="p-5 m-5 overflow-visible rounded-md">
@@ -52,26 +51,26 @@ export default function HelpView(pwops) {
 						How do i enable WoL?
 					</Text>
 					<Text>
-						Wake-on-Lan needs to be enabled at the BIOS level of a given device.
-						Usually, this particular option is found under for example "onboard
-						devices" or "PCIe devices" or a similar location. Alternatively,
-						some devices either allow or need WoL to be enabled though drivers.
-						Then, these settings can be accessed through the properties window
-						of the given device within your operating system.
+						Wake-on-Lan often need to be enabled at the BIOS level of a given
+						device. Usually, this particular option is found under for example
+						"onboard devices", "PCIe devices" or a similar location.
+						Alternatively, some devices either allow or need WoL to be enabled
+						though drivers. These settings can be accessed through the
+						properties window of the given device within your operating system.
 					</Text>
 					<Text>
-						Detailed information can be easily found for your given device and /
-						or operating system online:
-						<Text
-							style={{ color: "blue" }}
-							onPress={() =>
-								Linking.openURL(
-									"https://www.google.com/search?q=how+to+turn+on+Wake-on-Lan"
-								)
-							}
-						>
-							How to turn on Wake-on-Lan
-						</Text>
+						Detailed information can be found for your given device and / or
+						operating system online:
+					</Text>
+					<Text
+						style={{ color: "blue" }}
+						onPress={() =>
+							Linking.openURL(
+								"https://www.google.com/search?q=how+to+turn+on+Wake-on-Lan"
+							)
+						}
+					>
+						How to turn on Wake-on-Lan
 					</Text>
 				</Surface>
 			</ScrollView>
